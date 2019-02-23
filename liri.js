a5eae4ea10a050114ab16a13bad11db3249052ba
DESKTOP-GOA90O0\Theo Basallaje <theobasallaje@gmail.com>
2019-02-22 23:31:15 -0600

    added bonus

--- liri.js
+++ liri.js
@@ -6,7 +6,11 @@ var moment = require("moment");
 var fs = require("fs");
 const command = process.argv[2];
 const selection = process.argv.slice(3).join(" ");
-var movieName = process.argv.slice(3).join("+");
+var movieName = process.argv.slice(3).join(" ");
+var concertResponse;
+var spotifyResponse;
+var movieResponse;
+// var doWhatResponse;
 
 // var spotify = new Spotify({
 //   id: "a1a2532a8679426198646ae20386ab46",
@@ -26,10 +30,36 @@ function spotifyCommand(selection) {
     }
     var base = data.tracks.items[0]
 
-    console.log("Artists : " + base.artists[0].name);
-    console.log("Song Name : " + base.name);
-    console.log("Preview URL : " + base.preview_url);
-    console.log("Album Name : " + base.album.name);
+    // spotifyResponse = {
+    //   artist: base.artists[0].name,
+    //   songName: base.name,
+    //   previewUrl: base.preview_url,
+    //   albumName: base.album.name
+    // }
+
+    spotifyResponse = "//////////////////////RESULTS FROM SPOTIFY FOR " + selection.toLocaleUpperCase() + "/////////////////////\n"
+      + "Artist(s) : " + base.artists[0].name + "\n"
+      + "Song Name : " + base.name + "\n"
+      + "Preview URL : " + base.preview_url + "\n"
+      + "Album Name : " + base.album.name + "\n\n";
+
+    console.log(spotifyResponse);
+    writeToFile(spotifyResponse);
+
+    // console.log("Artist(s) : " + base.artists[0].name);
+    // console.log("Song Name : " + base.name);
+    // console.log("Preview URL : " + base.preview_url);
+    // console.log("Album Name : " + base.album.name);
+  });
+}
+
+function writeToFile(commandResponse) {
+  fs.appendFile("log.txt", commandResponse, function (error) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("No errors Writting the above response to log.tx, Check it out!");
+    }
   });
 }
 
@@ -39,10 +69,18 @@ if (command === "spotify-this-song") {
   var url = "https://rest.bandsintown.com/artists/" + selection + "/events?app_id=codingbootcamp";
   // console.log("you are in concert-this");
   axios.get(url).then(function (data) {
-    console.log(data.data[0].venue.name);
-    console.log(data.data[0].venue.country);
+    // console.log(data.data[0].venue.name);
+    // console.log(data.data[0].venue.country);
     var date = moment(data.data[0].datetime).format("MM/DD/YYYY");
-    console.log(date);
+    // console.log(date);
+
+    concertResponse = "//////////////////////RESULTS FROM BANDS IN TOWN FOR " + selection.toLocaleUpperCase() + "/////////////////////\n"
+      + "Venue : " + data.data[0].venue.name + "\n"
+      + "Country : " + data.data[0].venue.country + "\n"
+      + "Date : " + date + "\n\n";
+
+    console.log(concertResponse);
+    writeToFile(concertResponse);
   })
 } else if (command === "movie-this") {
   if (movieName === "") {
@@ -51,21 +89,34 @@ if (command === "spotify-this-song") {
   var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   axios.get(queryUrl).then(
     function (response) {
-      console.log("Title: " + response.data.Title);
-      console.log("Came out in: " + response.data.Year);
-      console.log("imdb rating: " + response.data.imdbRating);
-      console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
-      console.log("Produced in: " + response.data.Country);
-      console.log("Language: " + response.data.Language);
-      console.log("Plot: " + response.data.Plot);
-      console.log("Actors: " + response.data.Actors);
+      // console.log("Title: " + response.data.Title);
+      // console.log("Came out in: " + response.data.Year);
+      // console.log("imdb rating: " + response.data.imdbRating);
+      // console.log("Rotten Tomatoes: " + response.data.Ratings[1].Value);
+      // console.log("Produced in: " + response.data.Country);
+      // console.log("Language: " + response.data.Language);
+      // console.log("Plot: " + response.data.Plot);
+      // console.log("Actors: " + response.data.Actors);
+
+      movieResponse = "//////////////////////RESULTS FROM OMDB FOR " + movieName.toLocaleUpperCase() + "/////////////////////\n"
+        + "Title : " + response.data.Title + "\n"
+        + "Came out in : " + response.data.Year + "\n"
+        + "imdb rating : " + response.data.imdbRating + "\n"
+        + "Rotten Tomatoes : " + response.data.Ratings[1].Value + "\n"
+        + "Produced in : " + response.data.Country + "\n"
+        + "Language : " + response.data.Language + "\n"
+        + "Plot : " + response.data.Plot + "\n"
+        + "Actors : " + response.data.Actors + "\n\n";
+
+      console.log(movieResponse);
+      writeToFile(movieResponse);
     }
   );
 } else if (command === "do-what-it-says") {
   fs.readFile("random.txt", "utf8", function (error, data) {
     // If the code experiences any errors it will log the error to the console.
     if (error) {
-      return console.log(error);
+      return error;
     } else {
       data = data.split(" ");
       // console.log(data.slice(3).join(" "));
@@ -73,6 +124,8 @@ if (command === "spotify-this-song") {
     }
   });
 }
+
+
 
 
 
